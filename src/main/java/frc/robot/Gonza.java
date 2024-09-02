cf0bf1959c1aed3a8562ee63350de87b811ded15
Jose Pablo Lopez Rueda <josepablolopezrueda@Joses-MacBook-Air-5.local>
2024-09-02 13:19:02 -0600

    segundo commit por pdjo

--- src/main/java/frc/robot/Gonza.java
+++ src/main/java/frc/robot/Gonza.java
@@ -1,5 +1,66 @@
 package frc.robot;
 
-public class Gonza {
-    
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Gonza extends TimedRobot {
+
+  CANSparkMax motor1 = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax motor2 = new CANSparkMax(2, MotorType.kBrushless);
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    motor1.burnFlash(); // REINICIAR SPARK
+    motor2.setInverted(false); // INVERTIR SPARK 
+    /**
+     * CAMBIO A FALSE (28/08)
+     */
+    motor1.follow(motor2); // FOLLOWERS
+  }
+  
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
