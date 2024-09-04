fab1e149d45e40e903af56f11a25581a631e032b
binivazqua <binizavazquez@gmail.com>
2024-09-04 13:08:17 -0600

    Binini

---
+++ src/main/java/frc/robot/Binini.java
@@ -0,0 +1,49 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Binini extends TimedRobot{
+
+     /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+  
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+    
+}
